3b83a4db1fb8c6b3081c61a11f1ee29dbc500228
Samuel Darras <Raiden-sam@live.fr>
2020-01-28 11:34:45 +0100

    Security is now complete

--- src/Service/MailService.php
+++ src/Service/MailService.php
@@ -38,6 +38,7 @@ class MailService {
         $this->view = $view;
         $this->templatesLinks = [
             "resetPassword" => "mail/resetPassword.html.twig",
+            "activationLink" => "mail/activationLink.html.twig",
         ];
     }
 
