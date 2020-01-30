3f1184eb0308309c3b8f3f7f9ab33b2125548009
Samuel Darras <Raiden-sam@live.fr>
2020-01-30 11:51:09 +0100

    Add message in the template if no result

--- src/Controller/OfferController.php
+++ src/Controller/OfferController.php
@@ -11,6 +11,7 @@ use App\Repository\OfferRepository;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
