d02988692a06ac840559cf32af8d8915437cfbf1
akroma59 <vincentrichardeau@laposte.net>
2020-02-10 12:31:05 +0100

    Finish map

---
+++ src/Service/MapService.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Service;
+
+
+use App\Entity\Business;
+use App\Entity\Offer;
+use Symfony\Component\HttpClient\HttpClient;
+
+class MapService {
+
+    function getMap(object $object): array
+    {
+    
+        $location = $object->getLocation();
+        $client = HttpClient::create();
+        $response = $client->request('GET', "https://maps.googleapis.com/maps/api/geocode/json?address=$location&key=");
+        $statusCode = $response->getStatusCode();
+        $content = json_decode($response->getContent());
+        if ($content->status === "ZERO_RESULTS") {
+            $lat = 48.866667;
+            $lng = 2.333333;
+        }
+
+        else {
+            $content = $content->results[0];
+            $lat = $content->geometry->location->lat;
+            $lng = $content->geometry->location->lng;
+        }
+
+        $informations = [
+            'lat' => $lat,
+            'lng' =>  $lng,
+        ];
+
+        return $informations;
+    }
+}
